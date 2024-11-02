c4ec9349c637cfaf64dd47fa34bb3015b8d643ba
Krystian Nowak <kr21032002@icloud.com>
2024-11-02 13:00:02 -0700

    update paths in tests

--- test/units/heap.test.js
+++ test/units/heap.test.js
@@ -1,5 +1,13 @@
-import Heap from '../../src/main/algo/heap'
-import Graph from '../../src/main/algo/graph'
+import Heap from '../../src/lib/algo/heap'
+import Graph from '../../src/lib/algo/graph'
+
+const removeDependencies = (queue, extracted) => {
+  queue.heap.forEach((node, i) => {
+    queue.heap[i].dependencies = queue.heap[i].dependencies.filter(
+      (d) => d !== extracted.id
+    )
+  })
+}
 
 test('heap operations', () => {
   const heap = new Heap()
@@ -26,16 +34,16 @@ test('heapify with custom comparator', () => {
 
   const edges = [
     { source: 1, target: 2 },
-    { source: 1, target: 3 },
-    { source: 2, target: 3 },
-    { source: 3, target: 4 },
+    { source: 1, target: 4 },
+    { source: 2, target: 4 },
+    { source: 4, target: 3 },
   ]
 
   //        2
   //       / \
   //   1 -    \
   //       \   \
-  //        - - 3 - 4
+  //        - - 4 - 3
 
   const g = new Graph(nodes, edges)
 
@@ -45,14 +53,6 @@ test('heapify with custom comparator', () => {
     g.buildQueue(),
     (a, b) => a.dependencies.length - b.dependencies.length
   )
-
-  const removeDependencies = (queue, extracted) => {
-    queue.heap.forEach((node, i) => {
-      queue.heap[i].dependencies = queue.heap[i].dependencies.filter(
-        (d) => d !== extracted.id
-      )
-    })
-  }
 
   let extracted = queue.peek()
   removeDependencies(queue, extracted)
@@ -67,12 +67,74 @@ test('heapify with custom comparator', () => {
   extracted = queue.peek()
   removeDependencies(queue, extracted)
   queue.extract()
-  expect(extracted.id).toBe(3)
+  expect(extracted.id).toBe(4)
 
   extracted = queue.peek()
   removeDependencies(queue, extracted)
   queue.extract()
-  expect(extracted.id).toBe(4)
+  expect(extracted.id).toBe(3)
 
   expect(() => queue.extract()).toThrow('Heap underflow')
+})
+
+test('basic usage with actual nodes', () => {
+  let nodes = [
+    {
+      id: '84ea1fb2-11db-46ec-9c64-5881524372fc',
+      position: { x: 817, y: 198 },
+      prompt: 'asdf',
+      isConnectable: true,
+      type: 'prompt',
+      measured: { width: 661, height: 32 },
+      selected: false,
+      dragging: false,
+      dependencies: [],
+      context: [],
+    },
+    {
+      id: '584f0457-e35f-4d99-b813-35d4c3d2e040',
+      position: { x: 404, y: 178.625 },
+      prompt: 'asdf',
+      isConnectable: true,
+      type: 'constant',
+      measured: { width: 661, height: 32 },
+      selected: false,
+      dragging: false,
+      dependencies: ['9f52cb6b-ffbd-41c8-98b3-7d492ed77e94'],
+      context: [],
+    },
+    {
+      id: '9f52cb6b-ffbd-41c8-98b3-7d492ed77e94',
+      position: { x: 635.375, y: 363 },
+      prompt: 'afsdasdf',
+      isConnectable: true,
+      type: 'prompt',
+      measured: { width: 661, height: 32 },
+      selected: true,
+      dragging: false,
+      dependencies: ['84ea1fb2-11db-46ec-9c64-5881524372fc'],
+      context: [],
+    },
+  ]
+
+  const queue = new Heap(
+    nodes,
+    (a, b) => a.dependencies.length - b.dependencies.length
+  )
+
+  let extracted = queue.extract()
+  expect(extracted.id).toBe('84ea1fb2-11db-46ec-9c64-5881524372fc')
+  removeDependencies(queue, extracted)
+
+  extracted = queue.extract()
+  // make sure no dependecies are left
+  expect(extracted.dependencies.length).toBe(0)
+  expect(extracted.id).toBe('9f52cb6b-ffbd-41c8-98b3-7d492ed77e94')
+  removeDependencies(queue, extracted)
+
+  extracted = queue.extract()
+  // make sure no dependecies are left
+  expect(extracted.dependencies.length).toBe(0)
+  expect(extracted.id).toBe('584f0457-e35f-4d99-b813-35d4c3d2e040')
+  removeDependencies(queue, extracted)
 })
